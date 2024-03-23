1190921ba8b1f78a924962d753154ad7c48bc90a
floAr <florian.uhde@posteo.de>
2024-03-23 11:02:59 +0100

    Add frame2 project files

--- frame2/api/index.tsx
+++ frame2/api/index.tsx
@@ -1,12 +1,10 @@
-import { Button, Frog, TextInput, parseEther } from 'frog'
+import { Button, Frog, TextInput } from 'frog'
 import { handle } from 'frog/vercel'
 
 // Uncomment to use Edge Runtime.
 // export const config = {
 //   runtime: 'edge',
 // }
-
-import { abi } from './abi.js'
 
 export const app = new Frog({
   assetsPath: '/',
@@ -19,7 +17,6 @@ app.frame('/', (c) => {
   const { buttonValue, inputText, status } = c
   const fruit = inputText || buttonValue
   return c.res({
-    action: '/finish',
     image: (
       <div
         style={{
@@ -56,44 +53,13 @@ app.frame('/', (c) => {
         </div>
       </div>
     ),
-    intents: [<Button value="apple">Apple</Button>,
-    <Button.Transaction target="/send-ether">Send Ether</Button.Transaction>,
-    <Button.Transaction target="/create">Create</Button.Transaction>,
-    <Button.Reset>Reset</Button.Reset>,
+    intents: [
+      <TextInput placeholder="Enter custom fruit..." />,
+      <Button value="apples">Apples</Button>,
+      <Button value="oranges">Oranges</Button>,
+      <Button value="bananas">Bananas</Button>,
+      status === 'response' && <Button.Reset>Reset</Button.Reset>,
     ],
-  })
-})
-
-
-app.frame('/finish', (c) => {
-  const { transactionId } = c
-  return c.res({
-    image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
-        Transaction ID: {transactionId}
-      </div>
-    )
-  })
-})
-app.transaction('/send-ether', (c) => {
-  console.log('send-ether')
-  // Send transaction response.
-  return c.send({
-    chainId: 'eip155:84532',
-    to: '0xF065F8F230bFe7f2DfcfE716Bded22Ace33a7d4B',
-    value: parseEther('0.01'),
-  })
-})
-
-app.transaction('/create', (c) => {
-  // Contract transaction response.
-  return c.contract({
-    abi,
-    chainId: 'eip155:84532',
-    functionName: 'registerNewFrame',
-    args: [],
-    to: '0xF065F8F230bFe7f2DfcfE716Bded22Ace33a7d4B',
-    value: parseEther("0.01")
   })
 })
 
